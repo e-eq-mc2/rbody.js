63583dfd0ee40f89cc3681f20c6d8a668b7432dc
e-eq-mc2 <nk.nishizawa@gmail.com>
2012-08-04 02:00:53 +0900

    run with 1024 bodies

---
+++ js/main.js
@@ -0,0 +1,246 @@
+/**
+ * @author naoki
+ */
+
+"use strict";
+
+$(function () {
+	var canvas = $("#webglCanvas").get(0);
+	canvas.width  = $("#canvasArea").width();
+	canvas.height = $("#canvasArea").height();	
+	
+	var gl = initGL(canvas);
+	var prgObj = initShaders(gl, "vertexShader", "fragmentShader");
+	
+	var rbSys = new RbodySystem();
+	
+	var att = {
+		vertex   : gl.getAttribLocation(prgObj, "vertex"  ),
+		normal   : gl.getAttribLocation(prgObj, "normal"  ),
+		texCoord : gl.getAttribLocation(prgObj, "texCoord")
+	};
+	var uni = {
+		projectionMatrix      : gl.getUniformLocation(prgObj, "projectionMatrix"     ),
+		modelViewMatrix       : gl.getUniformLocation(prgObj, "modelViewMatrix"      ), 
+		normalMatrix          : gl.getUniformLocation(prgObj, "normalMatrix"         ),
+		enableLighting        : gl.getUniformLocation(prgObj, "enableLighting"       ),
+		enableTexture         : gl.getUniformLocation(prgObj, "enableTexture"        ),
+		lightDirection        : gl.getUniformLocation(prgObj, "lightDirection"       ),
+		lightSourceDiffuse    : gl.getUniformLocation(prgObj, "lightSourceDiffuse"   ),
+		lightSourceAmbient    : gl.getUniformLocation(prgObj, "lightSourceAmbient"   ),
+		lightSourceSpecular   : gl.getUniformLocation(prgObj, "lightSourceSpecular"  ),
+		frontMaterialDiffuse  : gl.getUniformLocation(prgObj, "frontMaterialDiffuse" ),
+		frontMaterialAmbient  : gl.getUniformLocation(prgObj, "frontMaterialAmbient" ),
+		frontMaterialSpecular : gl.getUniformLocation(prgObj, "frontMaterialSpecular"),
+		color                 : gl.getUniformLocation(prgObj, "color"                ),
+		texSampler            : gl.getUniformLocation(prgObj, "texSampler"           )
+	};
+	
+	var radius = rbSys.radius;
+	var sphere = new SolidSphere(rbSys.radius, 12, 6);
+	var sphereBuf = {
+		vertex   : new ArrayBuffer3f(gl, att.vertex),
+		normal   : new ArrayBuffer3f(gl, att.normal),
+		texCoord : new ArrayBuffer2f(gl, att.texCoord),
+		index    : new ElementArrayBuffer1us(gl),
+		tex2D    : new Tex2DBuffer(gl, 0, uni.texSampler)
+	};
+	sphereBuf.vertex  .setBuffer(gl, sphere.vertex   );
+	sphereBuf.normal  .setBuffer(gl, sphere.normal   );
+	sphereBuf.texCoord.setBuffer(gl, sphere.texCoord );
+	sphereBuf.index   .setBuffer(gl, sphere.index    );
+	sphereBuf.tex2D   .setBuffer(gl, "img/sintai.png");
+
+	var width  = rbSys.radius * 2;
+	var height = rbSys.radius * 2;
+	var board = new Board(width, height);
+	var boardBuf = {
+		vertex   : new ArrayBuffer3f(gl, att.vertex),
+		texCoord : new ArrayBuffer2f(gl, att.texCoord),
+		index    : new ElementArrayBuffer1us(gl),
+		tex2D    : new Tex2DBuffer(gl, 1, uni.texSampler)
+	};
+	boardBuf.vertex  .setBuffer(gl, board.vertex   );
+	boardBuf.texCoord.setBuffer(gl, board.texCoord );
+	boardBuf.index   .setBuffer(gl, board.index    );
+	boardBuf.tex2D   .setBuffer(gl, "img/karada1.png");
+	
+	var cube = new WireCube([-1, -1, -1], [ 1,  1,  1]);
+	var cubeBuf = {
+		vertex : new ArrayBuffer3f(gl, att.vertex),
+		index  : new ElementArrayBuffer1us(gl)
+	};
+	cubeBuf.vertex.setBuffer(gl, cube.vertex);
+	cubeBuf.index .setBuffer(gl, cube.index );
+	
+	var MAX_LOOP = 5000;
+	var TIME_OUT =   10;
+	var loop  = 0;
+	var angle = 0;
+	var timer0 = new Timer();
+	var timer1 = new Timer();
+	var numBody = rbSys.body.length;
+
+	(function drawLoop() {
+		timer0.start();
+
+		gl.enable(gl.DEPTH_TEST);
+		gl.enable(gl.CULL_FACE);
+		gl.depthFunc(gl.LEQUAL);
+		gl.cullFace(gl.BACK);
+		gl.clearColor(1, 1, 1, 1);
+		gl.clearDepth(1);
+		gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+		
+		timer1.start();
+		rbSys.update();
+		timer1.stop();
+
+		var   pM = mat4.frustum(-1, 1, -1, 1, 5, 10);
+		var  mvM = mat4.identity(mat4.create());
+		mat4.translate(mvM, [0, 0, -7]); // mvM = mvM * T
+		mat4.rotate(mvM, deg2rad(   20), [1, 0, 0]); // mvM = mvM * R
+		mat4.rotate(mvM, deg2rad(angle), [0, 1, 0]); // mvM = mvM * R
+
+		// projection matrix
+		gl.uniformMatrix4fv(uni.projectionMatrix, false, pM);
+
+		// Shpere
+		gl.uniform1i(uni.enableLighting, 1);
+		gl.uniform1i(uni.enableTexture , 0);
+		gl.uniform3fv(uni.lightDirection      , [0.0, 0.0, 1.0     ]);
+		gl.uniform4fv(uni.lightSourceDiffuse  , [1.0, 1.0, 1.0, 1.0]);
+		gl.uniform4fv(uni.frontMaterialDiffuse, [1.0, 1.0, 1.0, 1.0]);
+		gl.uniform4fv(uni.frontMaterialAmbient, [0.1, 0.1, 0.1, 1.0]);
+		sphereBuf.vertex  .bind(gl);
+		sphereBuf.normal  .bind(gl);
+		sphereBuf.texCoord.bind(gl);
+		sphereBuf.index   .bind(gl);
+		sphereBuf.tex2D   .bind(gl);
+		for (var i=0; i < numBody; ++i) {
+			var bi = rbSys.body[i];
+			var mvMBody = bi.toGLM4x4();
+			mat4.multiply(mvM, mvMBody, mvMBody); // mvM(body) = mvM(global) * mvM(body)
+
+			gl.uniformMatrix4fv(uni.modelViewMatrix, false,             mvMBody);
+			gl.uniformMatrix3fv(uni.normalMatrix   , false, normalMat3(mvMBody));
+			gl.drawElements(gl.TRIANGLES, sphere.index.length, gl.UNSIGNED_SHORT, 0);
+		}
+		sphereBuf.vertex  .unbind(gl);
+		sphereBuf.normal  .unbind(gl);
+		sphereBuf.texCoord.unbind(gl);
+		sphereBuf.index   .unbind(gl);
+		sphereBuf.tex2D   .unbind(gl);
+
+		/*
+		// Board
+		gl.uniform1i(uni.enableLighting, 0);
+		gl.uniform1i(uni.enableTexture , 1);
+		gl.uniform4fv(uni.color, [0, 0, 0, 1]);
+		boardBuf.vertex  .bind(gl);
+		boardBuf.texCoord.bind(gl);
+		boardBuf.index   .bind(gl);
+		boardBuf.tex2D   .bind(gl);
+		for (var i=0; i < numBody; ++i) {
+			var bi = rbSys.body[i];
+			var mvMBody = bi.toGLM4x4();
+			mat4.multiply(mvM, mvMBody, mvMBody); // mvM(body) = mvM(global) * mvM(body)
+
+			gl.uniformMatrix4fv(uni.modelViewMatrix, false,             mvMBody);
+			gl.uniformMatrix3fv(uni.normalMatrix   , false, normalMat3(mvMBody));
+			gl.drawElements(gl.TRIANGLES, board.index.length, gl.UNSIGNED_SHORT, 0);
+		}
+		boardBuf.vertex  .unbind(gl);
+		boardBuf.texCoord.unbind(gl);
+		boardBuf.index   .unbind(gl);
+		boardBuf.tex2D   .unbind(gl);
+		*/
+
+		// Cube
+		gl.uniform1i(uni.enableLighting, 0);
+		gl.uniform1i(uni.enableTexture , 0);
+		gl.uniform4fv(uni.color, [0, 0, 0, 0.5]);
+		gl.uniformMatrix4fv(uni.modelViewMatrix , false, mvM);
+		cubeBuf.vertex.bind(gl);
+		cubeBuf.index .bind(gl);
+		gl.drawElements(gl.LINES, cube.index.length, gl.UNSIGNED_SHORT, 0);
+		cubeBuf.vertex.unbind(gl);
+		cubeBuf.index .unbind(gl);
+		
+		gl.flush();
+		
+		timer0.stop();
+		var err = gl.getError();
+		if (err != gl.NO_ERROR && err != gl.CONTEXT_LOST_WEBGL) {
+			alert( WebGLDebugUtils.glEnumToString(err) );
+		}
+
+		++loop;
+		
+		var ela0 = Math.ceil(timer0.elapsedMsec());
+		var ave0 = Math.ceil(timer0.elapsedTotalMsec() / loop);
+		var ela1 = Math.ceil(timer1.elapsedMsec());
+		var ave1 = Math.ceil(timer1.elapsedTotalMsec() / loop);
+		
+		//angle += 360 / 20 * Math.max(ave0, TIME_OUT) / 1000;
+		
+		ela0 = num2str(ela0, 4);
+		ela1 = num2str(ela1, 4);
+		ave0 = num2str(ave0, 4);
+		ave1 = num2str(ave1, 4);
+		$("#info").html(
+			'<p>' + numBody + ' bodies, step: ' + loop + '</p>' +
+			'<p>' +  'R-body Elapsed: ' + ela1 + ' msec ' + '(ave. ' +  ave1 + ')' + '</p>' +
+			'<p>' +  '+WebGL Elapsed: ' + ela0 + ' msec ' + '(ave. ' +  ave0 + ')' + '</p>'
+		);
+
+		var timeoutId = setTimeout(drawLoop, TIME_OUT);
+		if (loop >= MAX_LOOP) clearTimeout(timeoutId);
+	} ());
+}); // end of $(document).ready(fuction(){});
+
+/* 
+ * 
+ * Utility
+ * 
+ */
+function deg2rad(deg) {
+	return deg * Math.PI / 180;
+}
+function num2str(num, digit) {
+	var str = num + "";
+	while (str.length < digit)
+		str = " " + str;
+	return str.replace(/ /g, "&nbsp;");
+}
+
+/* 
+ * 
+ * Timer
+ * 
+ */
+function Timer() {
+	this.reset();
+}
+Timer.prototype.reset = function() {
+	this.timeStart = 0;
+	this.timeStop = 0;
+	this.elapsed = 0;
+	this.elapsedTotal = 0;
+};
+Timer.prototype.start = function() {
+	this.timeStart = new Date().getTime();
+	return this.timeStart;
+};
+Timer.prototype.stop = function() {
+	this.timeStop = new Date().getTime();
+	this.elapsed = this.timeStop - this.timeStart;
+	this.elapsedTotal += this.elapsed;
+	return this.timeStop;
+};
+Timer.prototype.elapsedMsec = function() {
+	return this.elapsed;
+};
+Timer.prototype.elapsedTotalMsec = function() {
+	return this.elapsedTotal;
+};
---
+++ js/webgl-util.js
@@ -0,0 +1,383 @@
+/**
+ * @author nk.nishizawa@gmail.com
+ */
+
+"use strict";
+	
+function initGL(canvas) {
+	var DEBUG_MODE = false;
+
+	var gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl"); // A || B : if (A == true) return A else return B
+	if ( !gl ) {
+		alert('ERROR: not support "webgl" | "experimental-webgl"');
+		return;
+	}
+	//set view port
+	gl.viewportWidth  = canvas.width ;
+	gl.viewportHeight = canvas.height;
+
+	if (DEBUG_MODE) {
+		return WebGLDebugUtils.makeDebugContext(gl);
+	} else {
+		return gl;
+	}
+}
+
+function initShaders(gl, idVertexShader, idFragmentShader) {
+	var vxsObj = createShaderObj(gl, idVertexShader  );
+	var fgsObj = createShaderObj(gl, idFragmentShader);
+	var prgObj = createProgramObj(gl, vxsObj, fgsObj);
+	
+	return prgObj;
+	
+	////////////////////
+	// local function //
+	////////////////////
+	function createShaderObj(gl, elemId) {
+		var elem = document.getElementById(elemId);
+		if ( !elem ) {
+			alert('ERROR: can not find element "id=' + elemId + '"');
+			return; // will return undefined
+		}
+		
+		// create shader object
+		var shaderObj;
+		console.log("webgl: create shader object ...");
+		switch( elem.type ) {
+			case "x-shader/x-vertex":
+				shaderObj = gl.createShader(gl.VERTEX_SHADER);
+				break;
+			case "x-shader/x-fragment":
+				shaderObj = gl.createShader(gl.FRAGMENT_SHADER);
+				break;
+			default:
+				alert('ERROR: element type must be "x-shader/(x-vertex|x-fragment)"');
+				return; // will return undefined
+		}
+		console.log("webgl: attach shader source code to shader object ...");
+		gl.shaderSource(shaderObj, elem.text);
+		console.log("webgl: compile shader source code ...");
+		gl.compileShader(shaderObj);
+		
+		// check shader object status (compile)
+		if ( !gl.getShaderParameter(shaderObj, gl.COMPILE_STATUS) ) {
+			alert(gl.getShaderInfoLog(shaderObj));
+			alert('ERROR: compile shader source code "element id=' + elem.id + ' type=' + elem.type + '"');
+			return; // will return undefined
+		}
+		return shaderObj;
+	}
+	
+	////////////////////
+	// local function //
+	////////////////////
+	function createProgramObj(gl, vxsObj, fgsObj) {
+		var prgObj = gl.createProgram();
+		console.log("webgl: attach shader object to program object ...");
+		gl.attachShader(prgObj, vxsObj);
+		console.log("webgl: attach shader object to program object ...");
+		gl.attachShader(prgObj, fgsObj);
+		console.log("webgl: link program(vertex&fragment shader) object ...");
+		gl.linkProgram(prgObj);
+		// check program object status (link)
+		if ( !gl.getProgramParameter(prgObj, gl.LINK_STATUS) ) {
+			alert(gl.getProgramInfoLog(prgObj));
+			alert("ERROR: link program object");
+			return; // will return undefined
+		}
+		console.log("webgl: install program object as current rendering state ...");
+	    gl.useProgram(prgObj);
+		return prgObj;
+	}
+};
+
+
+var ArrayBuffer = function () {/* base class */};
+ArrayBuffer.prototype.initialize = function (buffer, bufferUsage, attLocation, dataStride, dataType) {
+	this.buffer      = buffer;
+	this.bufferUsage = bufferUsage; 
+	this.attLocation = attLocation;
+	this.dataStride  = dataStride;
+	this.dataType    = dataType;
+};
+ArrayBuffer.prototype.setBuffer = function (gl, data) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, data, this.bufferUsage);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+};
+ArrayBuffer.prototype.bind = function (gl) {
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+	gl.enableVertexAttribArray(this.attLocation);
+	gl.vertexAttribPointer(
+		this.attLocation,
+		this.dataStride, 
+		this.dataType,
+		false, 0, 0
+	);
+};
+ArrayBuffer.prototype.unbind = function (gl) {
+	gl.disableVertexAttribArray(this.attLocation);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+};
+
+function ArrayBuffer3f(gl, attLocation) {
+	this.initialize(
+		//this.initizlize.call(this,
+		gl.createBuffer(),
+		gl.STATIC_DRAW,
+		attLocation,
+		3,
+		gl.FLOAT
+	);
+}
+ArrayBuffer3f.prototype = new ArrayBuffer();
+
+function ArrayBuffer2f(gl, attLocation) {
+	this.initialize(
+		//this.initizlize.call(this,
+		gl.createBuffer(),
+		gl.STATIC_DRAW,
+		attLocation,
+		2,
+		gl.FLOAT
+	);
+}
+ArrayBuffer2f.prototype = new ArrayBuffer();
+
+var ElementArrayBuffer = function () {/* base class */};
+ElementArrayBuffer.prototype.initialize = function (buffer, bufferUsage) {
+	this.buffer      = buffer;
+	this.bufferUsage = bufferUsage; 
+};
+ElementArrayBuffer.prototype.setBuffer = function (gl, data) {
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, data, this.bufferUsage);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+};
+ElementArrayBuffer.prototype.bind = function (gl) {
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.buffer);
+};
+ElementArrayBuffer.prototype.unbind = function (gl) {
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+};
+
+function ElementArrayBuffer1us(gl) {
+	this.initialize(
+		gl.createBuffer(),
+		gl.STATIC_DRAW
+	);
+}
+ElementArrayBuffer1us.prototype = new ElementArrayBuffer();
+
+function Tex2DBuffer(gl, texUnit, uniLocation) {
+	this.initialize(gl, texUnit, uniLocation);
+}
+Tex2DBuffer.prototype.initialize = function (gl, texUnit, uniLocation) {
+	this.img = new Image();
+	this.texObj  = gl.createTexture();
+	this.texUnit = texUnit;
+	this.uniLocation = uniLocation;
+};
+Tex2DBuffer.prototype.setBuffer= function (gl, src) {
+	var img    = this.img;
+	var texObj = this.texObj;
+	this.img.onload = function () {
+		console.log("webgl: Image finish loading ... " + new Date().toLocaleString());
+		
+		console.log("webgl: bind texture object to current TO(TEXTURE_2D) ...");
+		gl.bindTexture(gl.TEXTURE_2D, texObj);
+		console.log("webgl: attach image data to point(TEXTURE_2D) ...");
+		gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
+		
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);	
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+		//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR_MIPMAP_LINEAR);	
+		//gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
+	
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
+		gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
+		
+		console.log("webgl: generate mipmap ...");
+		gl.generateMipmap(gl.TEXTURE_2D);
+		
+		console.log("webgl: un-bind texture object ...");
+		gl.bindTexture(gl.TEXTURE_2D, null);
+	};
+	this.img.src = src;
+	console.log("webgl: Image begin loading .... " + new Date().toLocaleString());
+};
+Tex2DBuffer.prototype.bind = function (gl) {
+	gl.activeTexture(gl[ "TEXTURE" + this.texUnit ]);
+	gl.bindTexture(gl.TEXTURE_2D, this.texObj);
+	
+	gl.uniform1i(this.uniLocation, this.texUnit);
+};
+Tex2DBuffer.prototype.unbind = function (gl) {
+	gl.activeTexture(gl.TEXTURE0);
+	//gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+function Mat4Stack() {
+	var _ary = new Array();
+	_ary.push( mat4.identity(mat4.create()) );
+	this.push = function () {
+		var last = _ary[_ary.length-1];
+		_ary.push( mat4.create(last) );
+	};
+	this.pop = function () {
+		_ary.pop();
+	};
+	this.top = function () {
+		return _ary[_ary.length-1];
+	};
+}
+
+function normalMat3(m) {
+	var invM = mat4.toInverseMat3(m);
+	return mat3.transpose(invM); // nM = ( mvM ^-1 ) ^t
+}
+
+function SolidSphere(radius, divX, divY) {
+	var numX = divX + 1; // num vertex x
+	var numY = divY + 1; // num vertex y
+	this.vertex   = new Float32Array(numX * numY * 3);
+	this.normal   = new Float32Array(numX * numY * 3);
+	this.texCoord = new Float32Array(numX * numY * 2);
+	this.index    = new Uint16Array(divX * divY * 2 * 3);
+	// Y
+	// ^
+	// |
+	// | 
+	// +------> X
+	// vertex & normal
+	var dTheta = 2.0 * Math.PI / divX; // 0 -> 360 degrees
+	var dPhi   =       Math.PI / divY; // 0 -> 180 degrees
+	for (var j=0, ii=0; j <= divY; ++j) {
+		var phi = j * dPhi ;
+		var y = radius * Math.cos(phi);
+		var r = radius * Math.sin(phi);
+		for (var i=0; i <= divX; ++i) {
+			var theta = i * dTheta;
+			var x = r * Math.cos(theta);
+			var z = r * Math.sin(theta);
+			var invL = Math.sqrt(x*x + y*y + z*z);
+			this.vertex[ii  ] = x;
+			this.vertex[ii+1] = y;
+			this.vertex[ii+2] = z;
+			this.normal[ii  ] = x * invL;
+			this.normal[ii+1] = y * invL;
+			this.normal[ii+2] = z * invL;
+			ii += 3;
+		}
+	}
+	// texture
+	var du = 1 / divX * 2;
+	var dv = 1 / divY * 2;
+	for (var j=0, ii=0; j <= divY; ++j) {
+		var v = j * dv;
+		for (var i=0; i <= divX; ++i) {
+			var u = i * du;
+			this.texCoord[ii++] = u;
+			this.texCoord[ii++] = v;
+		}
+	}
+	// index
+	for (var j = 0, ii=0; j < divY; ++j) {
+		for (var i = 0; i < divX; ++i) {
+			var v = j * numX + i;
+			// upper triangle
+			this.index[ii++] = v;
+			this.index[ii++] = v + 1;
+			this.index[ii++] = v + numX + 1;
+			// lower triangle
+			this.index[ii++] = v;
+			this.index[ii++] = v + numX + 1;
+			this.index[ii++] = v + numX;
+		}
+	}
+}
+
+function WireCube(x0, x1) {
+	this.vertex = new Float32Array(8 * 3);
+	this.index  = new Uint16Array(6 * 4 * 2);
+	for(var v=0, ii=0; v < 8; ++v) {
+		var x = v & 1 ? x1[0] : x0[0];
+		var y = v & 2 ? x1[1] : x0[1];
+		var z = v & 4 ? x1[2] : x0[2];
+		this.vertex[ii++] = x;
+		this.vertex[ii++] = y;
+		this.vertex[ii++] = z;
+	}
+	var face = new Array();
+	// face -x
+	face[0] = [0, 4, 6, 2];
+	// face +x
+	face[1] = [1, 3, 7, 5];
+	// face -y
+	face[2] = [0, 1, 5, 4];
+	// face +y
+	face[3] = [2, 6, 7, 3];
+	// face -z
+	face[4] = [0, 2, 3, 1];
+	// face +z
+	face[5] = [4, 5, 7, 6]; 
+	for(var f=0, ii=0; f < 6; ++f) { 
+		for(var v=0; v < 4; ++v) { 
+			var b = v; 
+			var e = ( b + 1 ) % 4;
+			this.index[ii++] = face[f][b]; 
+			this.index[ii++] = face[f][e]; 
+		}
+	}
+}
+
+function Board(width, height) {
+	this.vertex   = new Float32Array(4 * 3);
+	this.texCoord = new Float32Array(4 * 2); 
+	this.index    = new Uint16Array(6);
+
+	var hW = 0.5 * width ;
+	var hH = 0.5 * height;
+
+	var x0 = - hW;
+	var x1 = + hW;
+	var y0 = - hH;
+	var y1 = + hH;
+
+	var tri = [0, 1, 3, 0, 3, 2]; // 2(lower, upper) triangle index
+	// vertex, texture
+	for (var i = 0; i < 4; ++i) {
+		var swX = i & 1;
+		var swY = i & 2;
+		// vertex
+		// Y
+		// ^
+		// |
+		// | 
+		// +------> X
+		var x = swX ? x1 : x0;
+		var y = swY ? y1 : y0;
+		var z = 0;
+		var ii = i * 3;
+		this.vertex[ii  ] = x;
+		this.vertex[ii+1] = y;
+		this.vertex[ii+2] = z;
+
+		// texture
+		// +------> U
+		// |
+		// | 
+		// V
+		// V
+		var u = swX ? 1 : 0;
+		var v = swY ? 0 : 1;
+		ii = i * 2;
+		this.texCoord[ii  ] = u;
+		this.texCoord[ii+1] = v;
+	}
+	// index
+	for (var i = 0; i < 6; ++i) {
+		var ii = i * 1;
+		this.index[ii] = tri[i];
+	}
+}
